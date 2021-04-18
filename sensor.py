0cd4985316bf87054e5265cf248f89af021b1be6
uvejota <vmayor93@gmail.com>
2021-04-18 12:35:18 +0200

    prettying the code and testing what happens if we use a single api object

--- sensor.py
+++ sensor.py
@@ -110,6 +110,10 @@ class EDSSensor(Entity):
         self._attributes[ATTR_LOAD_NOW] = ""
         self._attributes[ATTR_POWER_LIMIT] = ""
 
+        # Login into the cloud platform
+        self._edis = Edistribucion(self._usr,self._pw,self._session)
+        self._edis.login()
+
     @property
     def name(self):
         """Return the name of the sensor."""
@@ -144,68 +148,66 @@ class EDSSensor(Entity):
     def reconnect_ICP (self):
         ### Untested... impossible under the current setup
         _LOGGER.debug("ICP reconnect service called")
-        # Login into the edistribucion platform. 
-        edis = Edistribucion(self._usr,self._pw,True)
-        edis.login()
         # Get CUPS list, at the moment we just explore the first element [0] in the table (valid if you only have a single contract)
-        r = edis.get_list_cups()
+        r = self._edis.get_list_cups()
         cups = r[0]['CUPS_Id']
         # Get response
-        response = edis.reconnect_ICP(cups)
+        response = self._edis.reconnect_ICP(cups)
         _LOGGER.debug(response)
 
     def update(self):
         """Fetch new state data for the sensor."""
-        # Login into the edistribucion platform. 
-        # TODO: try to save sessions by calling Edistribucion(self._usr,self._pw,True), for some reason this has been disabled until now
-        edis = Edistribucion(self._usr,self._pw,self._session)
-        edis.login()
+
         # Get CUPS list, at the moment we just explore the first element [0] in the table (valid if you only have a single contract)
-        r = edis.get_list_cups()
+        r = self._edis.get_list_cups()
         cups = r[0]['CUPS_Id']
         cont = r[0]['Id']
 
         self._attributes['CUPS'] = r[0]['CUPS'] # this is the name
-        #self._attributes['Cont'] = cont # not really needed
 
         # First retrieve historical data if first boot or starting a new day (this is fast)
         if self._is_first_boot or self._do_run_6am_tasks:
             _LOGGER.debug("fetching historical data")
+            # prepare some datetimes...
             yesterday = (datetime.today()-timedelta(days=1)).strftime("%Y-%m-%d")
             sevendaysago = (datetime.today()-timedelta(days=8)).strftime("%Y-%m-%d")
             onemonthago = (datetime.today()-timedelta(days=30)).strftime("%Y-%m-%d")
-
-            yesterday_curve=edis.get_day_curve(cont,yesterday)
-            self._attributes[ATTR_CONSUMPTION_YESTERDAY] = str(yesterday_curve['data']['totalValue']) + ' kWh'
-            lastweek_curve=edis.get_week_curve(cont,sevendaysago)
-            self._attributes[ATTR_CONSUMPTION_7DAYS] = str(lastweek_curve['data']['totalValue']) + ' kWh'
-            lastmonth_curve=edis.get_month_curve(cont,onemonthago)
-            self._attributes[ATTR_CONSUMPTION_30DAYS] = str(lastmonth_curve['data']['totalValue']) + ' kWh'
-
             thismonth = datetime.today().strftime("%m/%Y")
             ayearplusamonthago = (datetime.today()-timedelta(days=395)).strftime("%m/%Y")
-            maximeter_histogram = edis.get_year_maximeter (cups, ayearplusamonthago, thismonth)
+            # fetch historical data
+            yesterday_curve=self._edis.get_day_curve(cont,yesterday)
+            lastweek_curve=self._edis.get_week_curve(cont,sevendaysago)
+            lastmonth_curve=self._edis.get_month_curve(cont,onemonthago)
+            maximeter_histogram = self._edis.get_year_maximeter (cups, ayearplusamonthago, thismonth)
+            # store historical data as attributes
+            self._attributes[ATTR_CONSUMPTION_YESTERDAY] = str(yesterday_curve['data']['totalValue']) + ' kWh'
+            self._attributes[ATTR_CONSUMPTION_7DAYS] = str(lastweek_curve['data']['totalValue']) + ' kWh'
+            self._attributes[ATTR_CONSUMPTION_30DAYS] = str(lastmonth_curve['data']['totalValue']) + ' kWh'
             self._attributes[ATTR_MAXPOWER_1YEAR] = maximeter_histogram['data']['maxValue']
 
         # Then retrieve real-time data (this is slow)
         _LOGGER.debug("fetching real-time data")
-        meter = edis.get_meter(cups)
+        meter = self._edis.get_meter(cups)
         self._attributes[ATTR_ICPSTATUS] = meter['data']['estadoICP']
         self._total_consumption = float(meter['data']['totalizador'])
         self._attributes[ATTR_CONSUMPTION_TODAY] = str(meter['data']['totalizador']) + ' kWh'
         self._attributes[ATTR_LOAD_NOW] = meter['data']['percent']
         self._attributes[ATTR_POWER_LIMIT] = str(meter['data']['potenciaContratada']) + ' kW'
         
-        # if new day, store consumption
+        # if new day, store consumption TODO fix bad scaling...
         _LOGGER.debug("doing internal calculus")
         if self._do_run_daily_tasks or self._is_first_boot:
+            # if a new day has started, store last total consumption as the base for the daily calculus
             self._total_consumption_yesterday = float(self._total_consumption)
-
+        # do the maths and update it during the day
         self._attributes[ATTR_CONSUMPTION_TODAY] = str(self._total_consumption - self._total_consumption_yesterday) + ' kWh'
 
-        self._state = meter['data']['potenciaActual']
-        
+        # at this point, we should have update all attributes
         _LOGGER.debug("Attributes updated for EDSSensor: " + self._attributes)
+
+        # Update the state of the Sensor
+        self._state = meter['data']['potenciaActual']
+        _LOGGER.debug("State updated for EDSSensor: " + self._state)
 
         # set flags down
         self._do_run_daily_tasks = False
